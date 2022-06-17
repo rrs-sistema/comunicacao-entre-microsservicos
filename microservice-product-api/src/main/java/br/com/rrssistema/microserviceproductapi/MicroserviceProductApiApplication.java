d1f28523ded2229ce7b990fe657a3f72a2e4b951
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-06-17 00:25:15 -0300

    Criando APi Auth e Product
    Criando apis de auth e products

---
+++ microservice-product-api/src/main/java/br/com/rrssistema/microserviceproductapi/MicroserviceProductApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rrssistema.microserviceproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProductApiApplication.class, args);
+	}
+
+}
