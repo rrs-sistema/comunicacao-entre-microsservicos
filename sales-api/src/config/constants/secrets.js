9a690adca1ad27aecc5ac1fcb5e440d474bb75e8
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-06-24 22:15:19 -0300

    Finalizando configuração em Sales-API

---
+++ sales-api/src/config/constants/secrets.js
@@ -0,0 +1,5 @@
+const env = process.env;
+
+export const MONGO_DB_URL = env.MONGO_DB_URL ? env.MONGO_DB_URL : 'mongodb://127.0.0.1:27017/sales-db';
+export const API_SECRET =  env.API_SECRET ? env.API_SECRET : 'YXV0aC1hcGktc2VjcmV0LWRldi0xNTMxMTE='; //auth-api-secret-dev-153111
+export const RABBIT_MQ_URL = env.RABBIT_MQ_URL ? env.RABBIT_MQ_URL : 'amqp://localhost:5672';
---
+++ sales-api/src/config/db/initialData.js
@@ -0,0 +1,53 @@
+import Order from "../../modules/sales/model/Order.js";
+
+export async function createInitialData() {
+    await Order.collection.drop();
+
+    await Order.create({
+        products: [
+            {
+                productId: 1000,
+                quantity: 3
+              },
+              {
+                productId: 1002,
+                quantity: 1
+              },
+              {
+                productId: 1003,
+                quantity: 2
+            },                       
+        ],
+        user: {
+            id: 'a1d2df5ase1dsd5dfd',
+            name: 'Riva Robert',
+            email: 'riva.robert@gmail.com',
+        },
+        status: 'APPROVED',
+        createAt: new Date(),
+        updatedAt: new Date(),
+    });
+    await Order.create({
+        products: [
+            {
+            productId: 1002,
+            quantity: 5
+            },
+            {
+            productId: 1003,
+            quantity: 4
+            },                       
+        ],
+        user: {
+            id: 'b6a1df9bnew9nbf6my',
+            name: 'Kamilly Ketuly',
+            email: 'kamilly.ke@gmail.com',
+        },
+        status: 'REJECTED',
+        createAt: new Date(),
+        updatedAt: new Date(),
+    });    
+
+    let initialData = await Order.find();
+    console.log(`Initial data was created: ${JSON.stringify(initialData, undefined, 4)}`);
+}
